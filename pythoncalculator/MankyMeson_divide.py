7f1efb5675824001d7ee2e19dbb7236a754d303f
Clio <cliokj22@gmail.com>
2022-12-08 12:19:07 +0000

    Added running MankyMeson_divide.py condition

--- pythoncalculator/MankyMeson_divide.py
+++ pythoncalculator/MankyMeson_divide.py
@@ -1,3 +1,7 @@
 def divide(x, z):
     print("Wooo, I'm dividing numbers!!! ~ {}/{}".format(x,z))
     return x / z
+
+if __name__=="__main__":
+    result = divide(10/2)
+    print(" = {}".format(result))
